841bf6cb6153ce53b9c8097d4ffa84ac8afceb7d
Sevastyan Rakhimov <Rakhimov.SE@gmail.com>
2021-06-28 23:32:35 +0300

    Created download_data.py

---
+++ download_data.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+from random import random
+
+from config import TOKEN
+from tinder_api import TinderAPI
+
+if __name__ == "__main__":
+    api = TinderAPI(TOKEN)
+
+    counter = 0
+    while True:
+        persons = api.nearby_persons()
+        for person in persons:
+            counter += 1
+            print(f'{counter}. {person.name}')
+            person.download_images(folder="./images/unclassified", sleep_max_for=random() * 3)
+            sleep(random() * 10)
+        sleep(random() * 10)
